d09bb5b73c97688d4d2a377d1196c4ad666ce6ed
keminformatica <keminformatica@gmail.com>
2026-08-03 10:36:40 -0300

    link

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Cole aqui a URL do seu Apps Script Web App (termina em /exec)
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxTxXwzCe4E6DAfBwGuJl3LhPE0-3vbFKWOtRG4DKWvGh9iAM99aQfk5r06PJQr5PsgQg/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbybGtqQQg1D9MAVBHmGn9qaBwH4sFgXJUQPnx6vfHNejU4nnQEZRg-gCiagmECgPJKf/exec';
